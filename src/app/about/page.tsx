9a8fc5a6160b316f41a5dd4fe15574dbf8f0091d
AustinNChristensen <austin.christensen37@gmail.com>
2026-03-14 19:28:57 -0600

    fix: update email to austin@christensenequity.com on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -98,11 +98,11 @@ export default function About() {
                                 ),
                         )}
                         <SocialLink
-                            href="mailto:austin@christensen.onl"
+                            href="mailto:austin@christensenequity.com"
                             icon={MailIcon}
                             className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
                         >
-                            austin@christensen.onl
+                            austin@christensenequity.com
                         </SocialLink>
                     </ul>
                 </div>
